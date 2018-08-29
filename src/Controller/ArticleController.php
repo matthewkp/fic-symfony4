6b1f5cf66e06a5fb5aee0ee955a86f5ecb7d9aba
Matthieu Kapetanos <matthieu@dailykiosk.com>
2018-08-29 21:01:36 +0200

    ex5

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ArticleType;
 
 class ArticleController extends AbstractController
 {
@@ -29,6 +29,18 @@ class ArticleController extends AbstractController
     {
         return $this->render('article.html.twig', [
             'content' => 'Mon premier article',
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add()
+    {
+        $form = $this->createForm(ArticleType::class);
+
+        return $this->render('add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextType::class)
+            ->add('datePublished', DateTimeType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
