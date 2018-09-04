b58309c96e4e777a67e722632b7dc26872745c92
Matthieu Kapetanos <matthieu@dailykiosk.com>
2018-09-04 21:09:24 +0200

    ex14

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Utils\Slugger;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +16,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class ArticleController extends AbstractController
+class ArticleController
 {
     /**
      * @Route("/{_locale}", name="homepage")
